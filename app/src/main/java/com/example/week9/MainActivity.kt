48568676ac0b31ab26b167f5d018a17610384263
hs-1971199-parkinyong <tnr1884@naver.com>
2023-10-29 17:27:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/week9/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.week9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        Firebase.auth.signInWithEmailAndPassword("hansung@gmail.com", "hansu1ng")
+            .addOnCompleteListener(this) {
+                if(it.isSuccessful) {
+                    textView.text = "sign-in success ${Firebase.auth.currentUser?.uid}"
+                }
+                else {
+                    textView.text = "sign-in fail"
+                }
+            }
+    }
+}
